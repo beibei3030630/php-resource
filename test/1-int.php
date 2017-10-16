db63049b9190878334efb2086bf6d016d2df907e
beibei <1686310541@qq.com>
2017-10-16 17:15:54 +0800

    10-16commit

---
+++ test/1-int.php
@@ -0,0 +1,21 @@
+<?php
+  $var="正整数:";
+  $int=256;
+  echo "$var$int";
+  echo "<br/>";
+  
+  $var ="负整数:";
+  $int ="-256";
+  echo "$var$int";
+  echo "<br/>";
+ 
+  $var="八进制数：";
+  $int=0124;
+  echo "$var$int";
+  echo "<br/>";
+
+  $var="十六位进制数：";
+  $int=1432;
+  echo $var,$int;
+  echo "<br/>";
+
