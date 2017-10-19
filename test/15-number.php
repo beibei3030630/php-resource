87a0e83e55db2396f4461a8c5cd3ee2e1cc372b2
beibei <1686310541@qq.com>
2017-10-19 17:05:57 +0800

    10/19commit

--- test/15-number.php
+++ test/15-number.php
@@ -200,3 +200,216 @@ echo $str3.'<br/>';
 //mt_rand($min,$max)产生$min到$max之间的随机数
 
 echo mt_rand(1,9);
+
+echo '<br/>';
+
+//四位随机数验证码。每个数字颜色不一样 
+//通过字符串拼接和mt_rand()函数的方法
+$validate ='<span style="color:rgb('.mt_rand(0,255).','.mt_rand(0,255).','.mt_rand(0,255).')">'.mt_rand(1,9).'</span>';
+$validate.='<span style="color:rgb('.mt_rand(0,255).','.mt_rand(0,255).','.mt_rand(0,255).')">'.mt_rand(1,9).'</span>';
+$validate.='<span style="color:rgb('.mt_rand(0,255).','.mt_rand(0,255).','.mt_rand(0,255).')">'.mt_rand(1,9).'</span>';
+$validate.='<span style="color:rgb('.mt_rand(0,255).','.mt_rand(0,255).','.mt_rand(0,255).')">'.mt_rand(1,9).'</span>';
+
+echo "<hr/>";
+echo $validate;
+
+echo "<br/>";
+echo "<br/>";
+
+//练习三位验证码
+$validate='<span style="color:rgb('.mt_rand(0,255).','.mt_rand(0,255).','.mt_rand(0,255).')">'.mt_rand(1,9).'</span>';
+$validate.='<span style="color:rgb('.mt_rand(0,255).','.mt_rand(0,255).','.mt_rand(0,255).')">'.mt_rand(1,9).'</span>';
+$validate.='<span style="color:rgb('.mt_rand(0,255).','.mt_rand(0,255).','.mt_rand(0,255).')">'.mt_rand(1,9).'</span>';
+
+
+echo $validate;
+
+echo "<hr/>";
+
+//1-9选择题
+
+$a=6;$a/=3;$a*=2;$a%=3;
+echo $a.=3;//13;
+
+echo "<hr/>";
+
+
+
+
+
+//比较运算符
+//== 等于运算符 TRUE，如果类型转换后 $a 等于 $b。
+//=== 全等运算符TRUE，如果 $a 等于 $b，并且它们的类型也相同。
+
+
+var_dump(
+  1==true, 
+  1=='1',
+  0==false,
+  0==null,
+  //以上都为true 类型不同 但是值相等
+  1===true, 
+  1==='1',
+  0===false
+  //类型不同 三等肯定不相等
+	);
+echo '<hr/>';
+
+//!= 不相等  !==不全等 
+
+var_dump(
+ 1!=true,//false;
+ 1!='1',//false;
+ 0!=false,//false;
+
+ 1!==true,//true;
+ 1!=='1'//true;
+);
+echo '<hr/>';
+
+//$a <=> $b  太空船运算符（组合比较符） 
+//PHP7开始提供。
+//左边大于右边 返回1 
+//两边相等 返回0
+//左边小于右边 返回-1
+
+
+var_dump(
+  1<=>0.5,//-1
+  1<=>1,//0
+  1<=>2,//1
+  0<=>null,//0
+  0<=>false//0
+);
+
+echo "<hr/>";
+var_dump(
+	'a'<=>'b',//-1
+	'b'<=>'a',//1
+	'a'<=>'a'//0
+	);
+
+
+//$a??$b??$c  NULL 合并操作符
+//PHP7开始提供。
+//返回从左往右第一个存在且不为 NULL 的操作数。如果都没有定义且不为 NULL，则返回 NULL。
+
+ $var='beibei';
+var_dump(
+	$abc??$def??$var, //'beibei'.未定义的变量当成null
+	null??$var??null,//跳过第一个null，拿到第二位的操作数
+	true??$var??'123',//true
+	0??false??null??'$var'//int  0
+	//0 和false 不会转换为null
+	);
+echo "<hr/>";
+//1-11选择题
+$a=4;
+$b=5;
+echo ++$a+$b--.'<br/>'; //$a->5  递减后置位 $b-- ->5 
+echo $a>$b; //$b ->4 true  int 1
+
+echo '<hr/>';
+
+
+
+
+
+
+
+//逻辑运算符  
+//&& 如果第一个表达式为false，那么结果就为false
+//那么第二个表达式就被短路（执行不到第二个表达式）
+echo'逻辑与'.'<br/>';
+$i=0;$j=5;
+if($i&&++$j){
+	echo 'true'.'<br/>';
+}else{
+	echo "false".'<br/>';
+};
+echo $i,'<br/>'.$j.'<br/>';//$i=0 ->flase。$j被短路，所以没有执行前置递增。
+
+echo "<br/>";
+
+
+
+//  || 如果第一个表达式为true，那么结果就为true
+//那么第二个表达式就被短路（执行不到第二个表达式）
+echo "逻辑或".'<br/>';
+$i=4;$j=6;
+if($i++||--$j){
+	echo 'true'.'<br/>';
+}else{
+	echo "false".'<br/>';
+};
+echo $i,'<br/>'.$j.'<br/>';//$j未改变
+
+echo "<br/>";
+
+
+//  $a xor $b   逻辑异或,如果 $a 或 $b 任一为 TRUE，但不同时是，返回TRUE。但如果两个都是TRUE,则结果为false
+echo "逻辑异或".'<br/>';
+var_dump(
+	true xor true, //false
+	false xor true,// true
+	true xor false,// true
+	false xor false//false
+);
+
+echo '逻辑非'.'<br/>';
+//逻辑非
+var_dump(
+   !true,//false
+   !false//true 
+	);
+
+echo "<hr/>";
+
+echo "小练习";
+$i=1;
+$j=0;
+var_dump(
+  !0,//boolean  true
+  ++$abcdef&&--$alksjdflk,//null可递增 不可递减 boolean false
+  --$i||--$j  //boolean true
+);
+
+//@。当将其放置在一个 PHP 表达式之前，该表达式可能产生的任何错误信息都被忽略掉。
+@var_dump($sdlkjflskdf); 
+echo "<hr/>";
+
+
+
+
+
+//三元运算符
+echo '三元运算符'.'<br/>';
+//exp1?exp2:exp3
+$username='king';
+echo $username=='king'?"true":"false";
+
+echo "<br/>";
+
+//特殊情况
+ $res=1?:'abc';//如果缺少第二个表达式 结果又为真 那返回的就是第一个表达式  
+ 
+echo $res.'<br/>';
+
+
+
+
+//运算符优先级
+//想改变优先级可以通过（）
+//有短路的情况优先考虑短路
+$i=1;$j=0;
+if($p=$i--||$j++){//=等于号的优先级很低 所以先运行递增递减在运行||，最后赋值给$p
+	echo "true";
+}else{
+	echo "false";
+}
+
+var_dump($i,$j,$p);//$i int 0 为true。$j短路，$j不执行后置递增。 
+//$j int 0 
+// $p->true
+
+
---
+++ test/16-flowControl.php
@@ -0,0 +1,7 @@
+<?php
+//分支语句
+//if(exp){
+//}
+//if else
+//if elseif else
+//if 嵌套语句
